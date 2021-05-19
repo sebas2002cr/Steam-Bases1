c936c9cce5df9d0b6824c23899c198d56a63bd7b
andrewIC-2019 <61808125+andrewIC-2019@users.noreply.github.com>
2021-05-18 18:30:22 -0600

    SP Lectura LISTOS

--- sp_Andres.sql
+++ sp_Andres.sql
@@ -1,42 +1,48 @@
 -- ----------------------
--- Srcipt #4 - Andrés
+-- Srcipt #5 - Andres
 -- Sp de Lectura
 -- Sp de Escritura
 -- ----------------------
 
+-- ///////////////////////
+-- LECTURA
+-- \\\\\\\\\\\\\\\\\\\\\\
+
 -- ----------------------
 -- Lectura 1: 
--- SP que muestre los datos de venta de los juegos que ofrece un publisher
+-- SP que muestre los datos de venta de los juegos y condiciones del publisher
+-- ordenados por el nombre del publisher
 -- ----------------------
 
-DROP PROCEDURE IF EXISTS viewPublisherContracts;
+DROP PROCEDURE IF EXISTS sp_viewPublisherContracts;
 
 DELIMITER $$
-CREATE PROCEDURE viewPublisherContracts(pCompany_name NVARCHAR(256))
+CREATE PROCEDURE sp_viewPublisherContracts()
 BEGIN
-	SELECT * FROM view_GameSalesPublisher WHERE company_name = pCompany_name;
+	SELECT * FROM view_GameSalesPublisher ORDER BY company_name;
 END $$
 DELIMITER ;
 
-CALL viewPublisherContracts('Rockstar North');
+CALL sp_viewPublisherContracts();
 
 -- ----------------------------------------------
 
 -- ----------------------
 -- Lectura 2: 
--- SP que muestre la informacion de los juegos comprados por el usuario
+-- SP que muestre la informacion de los juegos comprados por los usuarios
+-- ordenado por nombre de user
 -- ----------------------
 
-DROP PROCEDURE IF EXISTS viewPublisherContracts;
+DROP PROCEDURE IF EXISTS sp_viewUserGames;
 
 DELIMITER $$
-CREATE PROCEDURE viewUserGames(pUsername NVARCHAR(256))
+CREATE PROCEDURE sp_viewUserGames()
 BEGIN
-	SELECT * FROM view_userGames WHERE username = pUsername;
+	SELECT * FROM view_userGames ORDER BY username;
 END $$
 DELIMITER ;
 
-CALL viewUserGames('Andres Arias');
+CALL sp_viewUserGames();
 
 -- ----------------------------------------------
 
@@ -46,21 +52,21 @@ CALL viewUserGames('Andres Arias');
 -- ----------------------
 SELECT * FROM PurchaseOrder;
 
-DROP PROCEDURE IF EXISTS viewAllPurchase;
+DROP PROCEDURE IF EXISTS sp_viewAllPurchase;
 
 DELIMITER $$
-CREATE PROCEDURE viewAllPurchase()
+CREATE PROCEDURE sp_viewAllPurchase()
 BEGIN
 	SELECT po.idPurchaseOrder AS `order`, po.amount, po.quantity, po.mail, po.`date`, -- podria ir a view
     ic.`name` AS article, po.idForOrder AS numArticle,
-    u.`name` AS buyer, u1.`name` AS receiver FROM PurchaseOrder po
+    CONCAT(u.`name`,' ',u.lastname) AS buyer, CONCAT(u1.`name`,' ',u1.lastname) AS receiver FROM PurchaseOrder po
     INNER JOIN item_category ic ON po.item_category_Id = ic.item_category_Id
     INNER JOIN Users u ON po.buyerId = u.userid
     INNER JOIN Users u1 ON po.itemForId = u1.userid;
 END $$
 DELIMITER ;
 
--- SELECT u.`name` AS buyer, u1.`name` AS receiver FROM PurchaseOrder po INNER JOIN Users u ON po.buyerId  = u.userid INNER JOIN Users u1 ON po.itemForId = u1.userid;
+CALL sp_viewAllPurchase;
 
 -- ----------------------
 -- Lectura 4:  
@@ -69,16 +75,18 @@ DELIMITER ;
 
 SELECT * FROM PaymentsAttempts;
 
-DROP PROCEDURE IF EXISTS listPaymentsAttempts():
+DROP PROCEDURE IF EXISTS sp_listPaymentsAttempts():
 
 DELIMITER $$
--- BEGIN
+CREATE PROCEDURE sp_listPaymentsAttempts()
+BEGIN
 	SELECT pa.paymentAttemptId, pa.posttime, pa.referenceNumber, pa.currencySymbol, pa.amount, -- podria ir a view
     m.`name` AS merchant, ps.`name` AS `status`, ic.`name` AS category, pa.idToPay AS articleId, pa.userid FROM PaymentsAttempts pa
     INNER JOIN Merchants m ON pa.merchantId = m.merchantId
     INNER JOIN PaymentStatus ps ON pa.idPaymentStatus = ps.idPaymentStatus
     INNER JOIN item_category ic ON pa.item_category_Id = ic.item_category_Id
     ORDER BY posttime DESC;
--- END$$
+END$$
 DELIMITER ;
 
+CALL sp_listPaymentsAttempts;
